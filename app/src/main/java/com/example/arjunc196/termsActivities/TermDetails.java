46fced263a303e51f88c621752f6c8608709d471
Arjun Esguerra <arjundesguerra@gmail.com>
2023-03-20 22:05:13 -0700

    delete term

--- app/src/main/java/com/example/arjunc196/termsActivities/TermDetails.java
+++ app/src/main/java/com/example/arjunc196/termsActivities/TermDetails.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.arjunc196.DatabaseHelper;
 import com.example.arjunc196.R;
@@ -17,6 +20,7 @@ public class TermDetails extends AppCompatActivity {
     private TextView termTitleTextView;
     private TextView startDateTextView;
     private TextView endDateTextView;
+    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,13 @@ public class TermDetails extends AppCompatActivity {
         termTitleTextView = findViewById(R.id.termNameDetails);
         startDateTextView = findViewById(R.id.startDateDetails);
         endDateTextView = findViewById(R.id.endDateDetails);
+        deleteButton = findViewById(R.id.deleteButton);
 
-        // Get the term ID passed through the intent
+        // get the term ID passed through the intent
         Intent intent = getIntent();
         long termId = intent.getLongExtra("term_id", -1);
 
-        // Fetch the term from the database using the term ID
+        // fetch the term from the database using the term ID
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String[] projection = {
                 "id AS _id",
@@ -44,7 +49,7 @@ public class TermDetails extends AppCompatActivity {
         String[] selectionArgs = { String.valueOf(termId) };
         Cursor cursor = db.query("terms", projection, selection, selectionArgs, null, null, null);
         if (cursor.moveToFirst()) {
-            // Display the term details in the UI
+            // display the term details in the UI
             String termTitle = cursor.getString(cursor.getColumnIndexOrThrow("termTitle"));
             String startDate = cursor.getString(cursor.getColumnIndexOrThrow("startDate"));
             String endDate = cursor.getString(cursor.getColumnIndexOrThrow("endDate"));
@@ -54,6 +59,20 @@ public class TermDetails extends AppCompatActivity {
             endDateTextView.setText(endDate);
         }
         cursor.close();
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // delete the term from the database
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                String selection = "id = ?";
+                String[] selectionArgs = { String.valueOf(termId) };
+                db.delete("terms", selection, selectionArgs);
+                Toast.makeText(TermDetails.this, "Term deleted successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
     }
 
     @Override
