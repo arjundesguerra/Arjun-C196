0b49a5a83cd9db3cc60479084a408bd5002181a9
Arjun Esguerra <arjundesguerra@gmail.com>
2023-03-23 13:17:31 -0700

    delete courses functions

--- app/src/main/java/com/example/arjunc196/coursesActivities/CourseDetails.java
+++ app/src/main/java/com/example/arjunc196/coursesActivities/CourseDetails.java
@@ -2,15 +2,82 @@ package com.example.arjunc196.coursesActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.arjunc196.DatabaseHelper;
 import com.example.arjunc196.R;
 
 public class CourseDetails extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
+    private TextView courseNameDetails;
+    private TextView startDateDetails;
+    private TextView endDateDetails;
+    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+        dbHelper = new DatabaseHelper(this);
+        courseNameDetails = findViewById(R.id.courseNameDetails);
+        startDateDetails = findViewById(R.id.startDateDetails);
+        endDateDetails = findViewById(R.id.endDateDetails);
+        deleteButton = findViewById(R.id.deleteButton);
+
+        // get the term ID passed through the intent
+        Intent intent = getIntent();
+        long termId = intent.getLongExtra("course_id", -1);
+
+        // fetch the term from the database using the term ID
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String[] projection = {
+                "id AS _id",
+                "courseTitle",
+                "courseStartDate",
+                "courseEndDate"
+        };
+        String selection = "id = ?";
+        String[] selectionArgs = { String.valueOf(termId) };
+        Cursor cursor = db.query("courses", projection, selection, selectionArgs, null, null, null);
+        if (cursor.moveToFirst()) {
+            // display the term details in the UI
+            String courseTitle = cursor.getString(cursor.getColumnIndexOrThrow("courseTitle"));
+            String courseStartDate = cursor.getString(cursor.getColumnIndexOrThrow("courseStartDate"));
+            String courseEndDate = cursor.getString(cursor.getColumnIndexOrThrow("courseEndDate"));
+
+            courseNameDetails.setText(courseTitle);
+            startDateDetails.setText(courseStartDate);
+            endDateDetails.setText(courseEndDate);
+        }
+        cursor.close();
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // delete the term from the database
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                String selection = "id = ?";
+                String[] selectionArgs = { String.valueOf(termId) };
+                db.delete("courses", selection, selectionArgs);
+                Toast.makeText(CourseDetails.this, "Term deleted successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
     }
 }
