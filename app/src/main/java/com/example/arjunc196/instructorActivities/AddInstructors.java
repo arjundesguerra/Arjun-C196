2eee46fa71de2a25b9571f68fdc63210b3134e41
Arjun Esguerra <arjundesguerra@gmail.com>
2023-03-24 14:20:44 -0700

    submit button adds to instructor database

--- app/src/main/java/com/example/arjunc196/instructorActivities/AddInstructors.java
+++ app/src/main/java/com/example/arjunc196/instructorActivities/AddInstructors.java
@@ -2,16 +2,27 @@ package com.example.arjunc196.instructorActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.arjunc196.DatabaseHelper;
 import com.example.arjunc196.R;
+import com.example.arjunc196.coursesActivities.AddCourses;
+import com.example.arjunc196.coursesActivities.CoursesList;
 
 public class AddInstructors extends AppCompatActivity {
 
     private EditText editInstructorName;
     private EditText editInstructorEmail;
     private EditText editInstructorNumber;
+    private Button submitButton;
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,7 @@ public class AddInstructors extends AppCompatActivity {
         editInstructorName = findViewById(R.id.editInstructorName);
         editInstructorEmail = findViewById(R.id.editInstructorEmail);
         editInstructorNumber = findViewById(R.id.editInstructorNumber);
+        submitButton = findViewById(R.id.submitButton);
 
         editInstructorName.setOnFocusChangeListener((v, hasFocus) -> {
             if (hasFocus) {
@@ -56,6 +68,28 @@ public class AddInstructors extends AppCompatActivity {
                     ((EditText) v).setText("Enter instructor phone number");
                 }
             }
+        });
+
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getReadableDatabase();
+
+        submitButton.setOnClickListener(view -> {
+            String name = editInstructorName.getText().toString();
+            String email = editInstructorEmail.getText().toString();
+            String number = editInstructorNumber.getText().toString();
+
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("instructorName", name);
+            contentValues.put("instructorEmail", email);
+            contentValues.put("instructorNumber", number);
+            db.insert("instructors", null, contentValues);
+
+            // open the instructor list activity
+            Intent intent = new Intent(AddInstructors.this, InstructorList.class);
+            startActivity(intent);
+
         });
 
 
