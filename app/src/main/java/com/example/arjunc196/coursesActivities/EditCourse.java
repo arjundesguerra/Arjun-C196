47f39ece58cadd84b0eeae07b7900b387b92efa6
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-01 13:41:56 -0700

    edit course title functionality

--- app/src/main/java/com/example/arjunc196/coursesActivities/EditCourse.java
+++ app/src/main/java/com/example/arjunc196/coursesActivities/EditCourse.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +27,7 @@ import java.util.Locale;
 public class EditCourse extends AppCompatActivity {
 
     private TextView courseTitleView;
+    private TextView editTitle;
     private Button editStatus;
     private Button editStartDate;
     private Button editEndDate;
@@ -53,6 +55,19 @@ public class EditCourse extends AppCompatActivity {
 
         courseTitleView = findViewById(R.id.courseTitleTextView);
         courseTitleView.setText(courseTitle);
+
+        editTitle = findViewById(R.id.editTitleText);
+        editTitle.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                // clear the text when the EditText gains focus
+                ((EditText) view).setText("");
+            } else {
+                // if the EditText loses focus, check if the text is empty and set it back to the original value
+                if (((EditText) view).getText().toString().isEmpty()) {
+                    ((EditText) view).setText("Enter a course title");
+                }
+            }
+        });
 
         editStatus = findViewById(R.id.statusButton);
         editStatus.setOnClickListener(view -> {
@@ -99,25 +114,34 @@ public class EditCourse extends AppCompatActivity {
         submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(view -> {
 
+            String titleText = editTitle.getText().toString();
             String statusText = editStatus.getText().toString();
             String startDateText = editStartDate.getText().toString();
             String endDateText = editEndDate.getText().toString();
 
             SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-            if (statusText.equals("Edit Status") || startDateText.equals("Edit Start Date") || endDateText.equals("Edit End Date")) {
+            if (titleText.equals("Edit Course Title") || statusText.equals("Edit Status") || startDateText.equals("Edit Start Date") || endDateText.equals("Edit End Date")) {
                 Toast.makeText(this, "All fields must be filled out", Toast.LENGTH_SHORT).show();
                 return;
             }
 
             // update the course information into the database row for the selected course
             ContentValues contentValues = new ContentValues();
+            ContentValues instructorContentValues = new ContentValues();
+            ContentValues noteContentValues = new ContentValues();
+
+            instructorContentValues.put("courseTitle", titleText);
+            noteContentValues.put("courseTitle", titleText);
+            contentValues.put("courseTitle", titleText);
             contentValues.put("status", statusText);
             contentValues.put("courseStartDate", startDateText);
             contentValues.put("courseEndDate", endDateText);
             String whereClause = "courseTitle=?";
             String[] whereArgs = new String[]{courseTitleView.getText().toString()};
+            db.update("instructors", instructorContentValues, whereClause, whereArgs);
             db.update("courses", contentValues, whereClause, whereArgs);
+            db.update("notes", noteContentValues, whereClause, whereArgs);
 
             finish();
         });
